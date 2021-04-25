eb64365eddcd385ecffea09006ead767078cab69
lichenbuliren <617106268@qq.com>
2021-04-25 10:34:16 +0800

    update alias config

--- vite.config.ts
+++ vite.config.ts
@@ -22,18 +22,7 @@ const config: UserConfigExport = {
   ],
   resolve: {
     alias: {
-      '@/': path.resolve(__dirname, './src'),
-      '@/config': path.resolve(__dirname, './src/config'),
-      '@/components': path.resolve(__dirname, './src/components'),
-      '@/styles': path.resolve(__dirname, './src/styles'),
-      '@/utils': path.resolve(__dirname, './src/utils'),
-      '@/common': path.resolve(__dirname, './src/common'),
-      '@/assets': path.resolve(__dirname, './src/assets'),
-      '@/pages': path.resolve(__dirname, './src/pages'),
-      '@/routes': path.resolve(__dirname, './src/routes'),
-      '@/layouts': path.resolve(__dirname, './src/layouts'),
-      '@/hooks': path.resolve(__dirname, './src/hooks'),
-      '@/stores': path.resolve(__dirname, './src/stores')
+      '@/': path.resolve(__dirname, './src/')
     }
   },
   css: {
@@ -54,15 +43,15 @@ const config: UserConfigExport = {
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
       // into your library
-      external: ['react', 'react-dom'],
-      output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDom'
-        }
-      }
+      // external: ['react', 'react-dom'],
+      // output: {
+      //   // Provide global variables to use in the UMD build
+      //   // for externalized deps
+      //   globals: {
+      //     react: 'React',
+      //     'react-dom': 'ReactDom'
+      //   }
+      // }
     }
   }
 }
